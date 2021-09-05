fb94c39085edc14aa9d6d4fbda56d1c8c7b4e5de
kimulchan <leerabi577@gmail.com>
2021-09-05 23:00:15 +0900

    ⚡:: cookie 추가

--- innocence-project/src/utils/cookie/cookie.js
+++ innocence-project/src/utils/cookie/cookie.js
@@ -1,5 +1,5 @@
 import {Cookies} from "react-cookie";
-
+import jwtDecode from "jwt-decode";
 const cookies= new Cookies();
 
 export const setCookie = (name,value,option)=>{
@@ -8,4 +8,18 @@ export const setCookie = (name,value,option)=>{
 
 export const getCookie = (name)=>{
     return cookies.get(name);
+}
+export const getData= (name)=>{
+    const token = getCookie(name);
+    const data = jwtDecode(token);
+    return data; 
+}
+export const getIsAdmin =(name)=>{
+    const data=getData(name);
+    return data.isAdmin;
+}
+
+export const getName =(name)=>{
+    const data=getData(name);
+    return data.name;
 }
